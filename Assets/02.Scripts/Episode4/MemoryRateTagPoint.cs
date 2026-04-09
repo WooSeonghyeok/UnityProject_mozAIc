57f8693fe3d92ee29004a87969dcfd765607e1ba
WooSeonghyeok <adamantfamily@gmail.com>
2026-04-09 19:37:24 +0900

    에피소드 4 기억 재구성 점수 시스템 구축
    1. TagPoint 오브젝트에 트리거 접촉 시 오브젝트에 설정한 점수만큼 획득
    2. ServerChat에 MemoryTag 키워드를 등록해 채팅 입력 시 키워드마다 등록된 점수만큼 획득

---
+++ Assets/02.Scripts/Episode4/MemoryRateTagPoint.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryRateTagPoint : MonoBehaviour
+{
+    readonly string playerTag = "Player";
+    private bool thisTagGet = false;
+    public string TagName;
+    public int memoryRateUp;
+    public GameObject tagBubble;
+    public CutsceneManager cutscene;
+    public string tagHint;
+    private void Awake()
+    {
+        tagBubble.SetActive(true);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(playerTag))
+        {
+            if (thisTagGet) return;
+            SaveManager.instance.curData.memory_reconstruction_rate += memoryRateUp;
+            foreach (IsTagGet tag in SaveManager.instance.curData.RateTag)
+            {
+                if (tag.TagName == TagName) tag.tagGet = true;
+            }
+            tagBubble.SetActive(false);
+            StartCoroutine(cutscene.TalkSay(CutsceneManager.TalkType.player, tagHint));
+            thisTagGet = true;
+        }
+    }
+}
