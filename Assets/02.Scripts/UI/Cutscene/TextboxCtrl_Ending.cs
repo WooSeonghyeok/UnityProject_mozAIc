a5f30cf985f7a6bf83ad49af6c007332e3e02a74
WooSeonghyeok <adamantfamily@gmail.com>
2026-04-13 10:34:29 +0900

    엔딩 신 연출 수정

--- Assets/02.Scripts/UI/Cutscene/TextboxCtrl_Ending.cs
+++ Assets/02.Scripts/UI/Cutscene/TextboxCtrl_Ending.cs
@@ -5,12 +5,15 @@ using static TextboxManager;
 public class TextboxCtrl_Ending : MonoBehaviour
 {
     public TextboxManager _manager;
+    public WaitForSecondsRealtime oneSec = new(1f);
     public WaitForSecondsRealtime twoSec = new(2f);
     public IEnumerator TrueEndCutscene()
     {
         yield return twoSec;
         StartCoroutine(_manager.TalkSay(TalkType.voice, "괜찮아. 이젠 전부 네 것이니까.", Talker.core));
         yield return twoSec;
+        StartCoroutine(_manager.TalkSay(TalkType.player, "기억은 사라진 게 아니었다."));
+        yield return oneSec;
         StartCoroutine(_manager.TalkSay(TalkType.player, "나는 그것을 견딜 수 있을 만큼 다시 돌아온 거다."));
     }
     public IEnumerator NormalEndCutscene()
@@ -18,6 +21,8 @@ public class TextboxCtrl_Ending : MonoBehaviour
         yield return twoSec;
         StartCoroutine(_manager.TalkSay(TalkType.voice, "괜찮아. 다 돌아오지 못했어도, 넌 여기까지 왔어.", Talker.core));
         yield return twoSec;
+        StartCoroutine(_manager.TalkSay(TalkType.player, "완전하지 않아도… 돌아올 수는 있다."));
+        yield return oneSec;
         StartCoroutine(_manager.TalkSay(TalkType.player, "아직 남은 빈칸이 있더라도, 그 역시 내 일부다."));
     }
 }
