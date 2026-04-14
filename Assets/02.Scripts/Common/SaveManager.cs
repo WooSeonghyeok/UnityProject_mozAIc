fe91bebb424681e31599a8d6d3b77a927c7c053c
WooSeonghyeok <adamantfamily@gmail.com>
2026-04-14 11:28:58 +0900

    기억 재구성 점수 기본값 조정
    에피소드 1의 퍼즐 재구성 점수 기본값 10점이 추가되는 시점을 '게임 시작' 시점에서 '에피소드 1 퍼즐 완료' 시점으로 이동

--- Assets/02.Scripts/Common/SaveManager.cs
+++ Assets/02.Scripts/Common/SaveManager.cs
@@ -110,7 +110,7 @@ public class SaveManager : MonoBehaviour
         dataObj.ep4_puzzle1Clear = false;
         dataObj.ep4_puzzle2Clear = false;
         dataObj.ep4_puzzle3Clear = false;
-        dataObj.memory_reconstruction_rate = 40;
+        dataObj.memory_reconstruction_rate = 30;
         dataObj.CoreTag = CreateDefaultMemoryTags();
         dataObj.npcInformations = new List<NPCInfo>();
         string[] npcNames = { "npc_ep1_luna", "npc_ep2_painter", "npc_ep3_musician", "npc_ep4_core" };
