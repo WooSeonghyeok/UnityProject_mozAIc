af9fd5600eba947480e1810caec3520317f49dbe
WooSeonghyeok <adamantfamily@gmail.com>
2026-04-14 20:02:31 +0900

    옵션 → 불러오기 / 그만하기 팝업 연결

--- Assets/02.Scripts/UI/Cutscene/TextboxCtrl_Ep4.cs
+++ Assets/02.Scripts/UI/Cutscene/TextboxCtrl_Ep4.cs
@@ -134,6 +134,7 @@ public class TextboxCtrl_Ep4 : MonoBehaviour
         yield return _manager.TalkSay(TalkType.voice, "넌 잊은 게 아니야.", 2f, Talker.core);
         yield return _manager.TalkSay(TalkType.voice, "버티기 위해, 잠시 나눠 둔 거야.", 1.5f, Talker.core);
         yield return _manager.TalkSay(TalkType.voice, "추억도, 꿈도, 사랑도… 전부 네가 감당해야 했던 삶이었어.", 1.5f, Talker.core);
+        yield return new WaitForSeconds(0.15f);
         climaxCam.Priority = 15;
         yield return new WaitForSeconds(0.15f);
         yield return _manager.TalkSay(TalkType.voice, "하지만 그때의 너는 너무 무너져 있었지.", 1.5f, Talker.core, true);
@@ -145,7 +146,7 @@ public class TextboxCtrl_Ep4 : MonoBehaviour
         climaxCam.Priority = 1;
         endSound.Play();
         yield return _manager.TalkSay(TalkType.player, "처음부터... 내 목소리였구나.", 2f);
-        yield return new WaitForSeconds(1.5f);  // 시네머신 변환 완료 대기
+        yield return new WaitForSeconds(1f);  // 시네머신 변환 완료 대기
         _manager.UserCtrl(true);
         otherCanvas.enabled = true;
     }
