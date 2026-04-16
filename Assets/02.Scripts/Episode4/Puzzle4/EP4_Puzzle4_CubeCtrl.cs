6ecce507ef288df6b159bdb2ed5e56e91640b04a
WooSeonghyeok <adamantfamily@gmail.com>
2026-04-16 13:39:21 +0900

    병합 후 에러 해결
    SwitchSet() 및 HintSet()이 겹치는 이슈
    Switch_Click 메소드 이름이 변경되지 않은 이슈

--- Assets/02.Scripts/Episode4/Puzzle4/EP4_Puzzle4_CubeCtrl.cs
+++ Assets/02.Scripts/Episode4/Puzzle4/EP4_Puzzle4_CubeCtrl.cs
@@ -54,23 +54,6 @@ public class EP4_Puzzle4_CubeCtrl : MonoBehaviour
         HintSet();
         if (Puzzle4Manager.instance != null) Puzzle4Manager.instance.RetryEvent += HandleRetry;
     }
-    private void SwitchSet()
-    {
-        if (colorSwitch != null)
-        {
-            colorSwitch.gameObject.SetActive(switchValue != 0);
-            colorSwitch.GetComponent<BoxCollider>().enabled = (switchValue != 0);
-        }
-    }
-    private void HintSet()
-    {
-        if (hint != null)
-        {
-            hint.SetActive(switchValue != 0);
-            hint_cond.GetComponent<Renderer>().material = CondSetup(condition);
-            hint_eff.GetComponent<Renderer>().material.color = EffColorSetup(switchValue);
-        }
-    }
     private void OnDisable()
     {
         if (Puzzle4Manager.instance != null) Puzzle4Manager.instance.RetryEvent -= HandleRetry;
@@ -94,6 +77,14 @@ public class EP4_Puzzle4_CubeCtrl : MonoBehaviour
         if (colorSwitch != null) SwitchSet();
         if (hint != null) HintSet();
         OnColorChanged?.Invoke();
+    }
+    private void SwitchSet()
+    {
+        if (colorSwitch != null)
+        {
+            colorSwitch.gameObject.SetActive(switchValue != 0);
+            colorSwitch.GetComponent<BoxCollider>().enabled = (switchValue != 0);
+        }
     }
     private void HintSet()
     {
@@ -121,14 +112,6 @@ public class EP4_Puzzle4_CubeCtrl : MonoBehaviour
         float greenV = (switchValue & 2) == 2 ? 1 : 0;
         float blueV = (switchValue & 4) == 4 ? 1 : 0;
         return new Color(redV, greenV, blueV);
-    }
-    private void SwitchSet()
-    {
-        if (colorSwitch != null)
-        {
-            colorSwitch.gameObject.SetActive(switchValue != 0);
-            colorSwitch.GetComponent<BoxCollider>().enabled = (switchValue != 0);
-        }
     }
     public void CubeDataSetup()
     {
