3db1e470f783f96735942359adb3adc0e4a17251
WooSeonghyeok <adamantfamily@gmail.com>
2026-04-20 13:58:50 +0900

    GameManager에 마우스 커서 고정 기능 추가
    + UI Input System을 GameManager에 연결

--- Assets/02.Scripts/Common/Manager/GameManager.cs
+++ Assets/02.Scripts/Common/Manager/GameManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
@@ -11,5 +12,22 @@ public class GameManager : MonoBehaviour
             AudioListener.volume = PlayerPrefs.GetFloat("Volume", 1f);
         }
         else Destroy(gameObject);
+        Cursor.lockState = CursorLockMode.Confined;  // 마우스 커서를 창 화면 내에 고정
+    }
+    public void OnCursorLock(InputAction.CallbackContext context)  //마우스 고정 버튼
+    {
+        if (context.started)
+        {
+            if (Cursor.lockState == CursorLockMode.Confined)
+            {
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+            else
+            {
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.Confined;
+            }
+        }
     }
 }
