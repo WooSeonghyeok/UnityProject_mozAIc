24e9ed8f7c5956431f24b773813b1b5366cd7c41
WooSeonghyeok <adamantfamily@gmail.com>
2026-04-20 10:18:23 +0900

    Scene / Script 폴더 정리
    * Scene별 리소스 파일(volume, navMesh, light)을 별도 폴더로 모음
    * 스크립트 계층화

---
+++ Assets/02.Scripts/Stage/Episode2/Space_Puzzle/ToggleObjectOnTrigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ToggleObjectOnTrigger : MonoBehaviour
+{
+    [Header("Toggle Target")]
+    public GameObject targetObject;
+    [Header("Settings")]
+    public string playerTag = "Player"; // 플레이어 태그
+    private bool isActive = false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag(playerTag)) return;
+        ToggleObject();
+    }
+    void ToggleObject()
+    {
+        if (targetObject == null) return;
+        isActive = !isActive;
+        targetObject.SetActive(isActive);
+        Debug.Log("토글 상태: " + isActive);
+    }
+}
