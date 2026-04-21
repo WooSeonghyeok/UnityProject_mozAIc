36213ffa435b6a6e832acf6c8713009bb1680a0c
WooSeonghyeok <adamantfamily@gmail.com>
2026-04-21 11:33:12 +0900

    Ep2 상호작용 가능한 트리거 콜라이더 배치

--- Assets/02.Scripts/Stage/Episode2/Common/EP2_InteractObject.cs
+++ Assets/02.Scripts/Stage/Episode2/Common/EP2_InteractObject.cs
@@ -36,17 +36,12 @@ public class EP2_InteractObject : MonoBehaviour
     }
     public void Interact()
     {
-        if (isUsed) return;
-
+        if (isUsed || !isContact) return;
         isUsed = true;
-
         Episode2ScoreManager.Instance?.AddInteractionScore(1);
-
         Debug.Log($"{gameObject.name} 상호작용 +1");
-
         // 🔥 선택: 다시 못 누르게
         GetComponent<Collider>().enabled = false;
-
         // 또는
         // gameObject.SetActive(false);
     }
