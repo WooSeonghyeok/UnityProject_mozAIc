76e6f2729559bcebb56c5b84e01faae08f7af917
mmanimalmm <mmanimalmm@Gmail.com>
2026-04-22 13:28:03 +0900

    PaintPuzzle 낙하속도연출 추가
    PaintPuzzle 낙하속도연출 추가

--- Assets/02.Scripts/Stage/Episode2/Paint_Puzzle/FallFOVEffect.cs
+++ Assets/02.Scripts/Stage/Episode2/Paint_Puzzle/FallFOVEffect.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using Cinemachine;
 
 public class FallFOVEffect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public CinemachineVirtualCamera vcam;
 
-    // Update is called once per frame
+    public float normalFOV = 40f;
+    public float fallFOV = 85f;
+
+    public float duration = 2f; // 변화 시간
+
+    private float t = 0f;
+    private bool isFalling = false;
+
     void Update()
     {
-        
+        if (vcam == null) return;
+
+        if (isFalling)
+        {
+            t += Time.deltaTime / duration;
+        }
+        else
+        {
+            t -= Time.deltaTime / duration;
+        }
+
+        t = Mathf.Clamp01(t);
+
+        // ⭐ 핵심: 가속 느낌
+        float smoothT = Mathf.SmoothStep(0f, 1f, t);
+
+        vcam.m_Lens.FieldOfView = Mathf.Lerp(normalFOV, fallFOV, smoothT);
+    }
+
+    public void StartFall()
+    {
+        isFalling = true;
+    }
+
+    public void StopFall()
+    {
+        isFalling = false;
     }
 }
