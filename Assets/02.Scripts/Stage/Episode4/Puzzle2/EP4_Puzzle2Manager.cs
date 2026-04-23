9bf2059beb14940a2c3376a5bf64e032d12e0f65
WooSeonghyeok <adamantfamily@gmail.com>
2026-04-23 17:53:21 +0900

    각 퍼즐 완료 시 컷신 출력 포인트 및 이미지 추가

--- Assets/02.Scripts/Stage/Episode4/Puzzle2/EP4_Puzzle2Manager.cs
+++ Assets/02.Scripts/Stage/Episode4/Puzzle2/EP4_Puzzle2Manager.cs
@@ -6,6 +6,7 @@ public class EP4_Puzzle2Manager : MonoBehaviour
     public GameObject gatePrefab; // 🔥 문
     bool isActivated = false;
     public TextboxCtrl_Ep4 cutscene;
+    public CutsceneImagePlayer puzzle2Cutscene;
     bool isMidCutsceneOn = false;
     private void Awake()
     {
@@ -23,6 +24,7 @@ public class EP4_Puzzle2Manager : MonoBehaviour
         if (obj1.activeSelf && obj2.activeSelf)  // 🔥 둘 다 활성화됐는지 체크
         {
             ActivatePortal();
+            puzzle2Cutscene.PlayCutscene();
             isActivated = true;
         }
     }
