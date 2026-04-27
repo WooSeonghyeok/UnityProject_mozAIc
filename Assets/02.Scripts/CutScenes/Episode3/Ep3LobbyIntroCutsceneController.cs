46c5a2d2bc702cdda78b81750ee598796cc79d9a
WooSeonghyeok <adamantfamily@gmail.com>
2026-04-27 16:55:17 +0900

    Update Ep3LobbyIntroCutsceneController.cs
    textboxManager 참조 누락 롤백

--- Assets/02.Scripts/CutScenes/Episode3/Ep3LobbyIntroCutsceneController.cs
+++ Assets/02.Scripts/CutScenes/Episode3/Ep3LobbyIntroCutsceneController.cs
@@ -1480,7 +1480,7 @@ public class Ep3LobbyIntroCutsceneController : MonoBehaviour
         {
             return;
         }
-
+        TextboxManager textboxManager = forceStandaloneSubtitleOverlay ? null : FindTextboxManager();
         Ep3CutsceneSubtitlePresenter[] presenters = FindObjectsOfType<Ep3CutsceneSubtitlePresenter>(true);
         if (presenters != null && presenters.Length > 0)
         {
